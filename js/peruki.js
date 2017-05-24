acf5cf402f465e4112925dd7b96804f6c66b3cf7
peruki <gota@mixedsign.com>
2017-05-24 13:02:36 +0300

    new site

---
+++ js/peruki.js
@@ -0,0 +1,18 @@
+<script type='text/javascript'>
+	$(document).ready(function () {
+    jQuery("#nanoDecors").nanogallery2({
+        kind: 'flickr',
+        userID: '154684446@N07',
+        photoset:'72157680902224464',
+            
+        thumbnailWidth: 185,
+        thumbnailHeight: 150,
+        thumbnailLabel: {
+            displayDescription: false,
+            position: 'overImageOnBottom'
+        },
+        thumbnailHoverEffect:'borderLighter'
+        
+    });
+	});
+	</script>
